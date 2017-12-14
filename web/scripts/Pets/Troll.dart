5a293a30d9872f2065a9a54906f9a1fad9ca1483
Jenny <gtg287y@gmail.com>
2017-12-14 14:03:48 -0500

    alright, pupated trolls are things. now just gotta make a pupatin page

--- web/scripts/Pets/Troll.dart
+++ web/scripts/Pets/Troll.dart
@@ -15,13 +15,21 @@ class Troll extends Pet{
     String type = Pet.TROLL;
     Troll(Doll doll, {health: 100, boredom: 0}) : super(doll, health: health, boredom: boredom) {
         //turns grub into troll., later will calc sign
-        window.alert("it's a motherfucking troll");
-        doll = Doll.convertOneDollToAnother(doll, new HomestuckTrollDoll());
+        this.doll = Doll.convertOneDollToAnother(doll, new HomestuckTrollDoll());
+        print("doll for troll is $doll");
 
     }
 
     Troll.fromJSON(String json, [JsonObject jsonObj]) : super(null){
         loadFromJSON(json, jsonObj);
+        this.doll = Doll.convertOneDollToAnother(doll, new HomestuckTrollDoll());
+        print("doll for troll is $doll");
         print ("loaded $name");
+    }
+
+    //returns where next thing should be
+    int drawTimeStats(CanvasElement textCanvas, int x, int y, int fontSize,buffer) {
+        Renderer.wrap_text(textCanvas.context2D,"I'm a motherfucking troll. Deal with it.",x,y,fontSize,400,"left");
+        return y;
     }
 }
--- web/scripts/Player/PetInventory.dart
+++ web/scripts/Player/PetInventory.dart
@@ -210,6 +210,7 @@ class PetInventory {
     }
 
     Future<CanvasElement> drawPet(Element container, Pet p, [CanvasElement canvas]) async {
+        print("drawing pet $p");
         DivElement canvasContainer = new DivElement();
         if(canvas == null) {
             canvas = new CanvasElement(width: p.textWidth, height: p.textHeight);
