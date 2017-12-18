cc12142dc34349bbe73b8d80d36bb77b1f41acf8
Jenny <gtg287y@gmail.com>
2017-12-18 15:17:03 -0500

    numbers are fucking banned

--- web/scripts/Pets/Stat.dart
+++ web/scripts/Pets/Stat.dart
@@ -1,8 +1,11 @@
 import 'package:DollLibCorrect/DollRenderer.dart';
 
 class Stat {
-    static int highNormal = 20;
-    static int lowNormal = -20;
+    static int HIGH = 20;
+    static int LOW = 0;
+    static int MEDIUM = 10;
+    static int VERYFUCKINGHIGH = 50;  //TODO figure out what's a rare but obtainable value once there's gameplay
+
     int value;
     String positiveName;
     String negativeName;
@@ -10,16 +13,26 @@ class Stat {
     Stat(this.value, this.positiveName, this.negativeName) {
         if(value == null) {
             Random rand = new Random();
-            value = rand.nextIntRange(lowNormal, highNormal);
+            value = rand.nextIntRange(-1* HIGH, HIGH); //won't go above medium normally except rarely
         }
+    }
+
+    int get normalizedValue => value.abs();
+
+    String get stringValue {
+        if(normalizedValue > VERYFUCKINGHIGH) return "Insanely High";
+        if(normalizedValue > HIGH) return "High";
+        if(normalizedValue > MEDIUM) return "Medium";
+        if(normalizedValue >= LOW) return "Low";
+        return "GLITCHED??? $normalizedValue";
     }
 
     @override
     String toString() {
         if(value >= 0) {
-            return "$positiveName: $value";
+            return "$positiveName: $stringValue";
         }else {
-            return "$negativeName: ${value.abs()}";
+            return "$negativeName: $stringValue";
         }
     }
 
