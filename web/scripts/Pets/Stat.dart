1707c3736ad6c234eb42a99b5c0d28c804c2a701
Jenny <gtg287y@gmail.com>
2017-12-20 10:57:13 -0500

    cause of death structure exists, now to wire up.

--- web/scripts/Pets/Stat.dart
+++ web/scripts/Pets/Stat.dart
@@ -25,15 +25,18 @@ class Stat {
     static StatFlavor freeFlavor;
     static StatFlavor internalFlavor;
 
-    //flavor will keep high, low, mediumm, very high, and caste shit
+    //flavor will keep high, low, mediumm, very high, and caste shit and causes of death
     StatFlavor positiveFlavor;
     StatFlavor negativeFlavor;
+
+
 
     //expect the template to look like "They "resented their role as a wiggler caretaker" and "stayed under the radar".
     static void makeFlavors() {
         Random rand = new Random();
 
-        defaultFlavor = new StatFlavor()
+        defaultFlavor = new StatFlavor(0.0)
+            ..death = <String>["of a mysterious illness","suddenly and with no warning"]
             ..jade = <String>["resented their role as a wiggler caregiver","cared for wigglers in the Caverns","flourished in their role as a wiggler caregiver","discovered they were a Rainbow Drinker after a tragic accident"]
             ..fuchsia = <String>["lived a life of hedonistic ${rand.pickFrom(<String>["cake baking","movie stardom","hilarious culling"])} ","reformed ${rand.pickFrom(<String>["culling policies","education","warfare"])}","helped the citizens of the empire as best they can","planned their rebellion against the Empress"]
             ..purple = <String>["learned how to be a devout member of the Dark Carnival","memorized scripture on the Mirthful Messiahs","ignored the Juggalo Church entirely", "subjuggulated the lower bloods", "excelled as a Laughsassin"]
@@ -44,30 +47,35 @@ class Stat {
             ..low = <String>["stayed under the radar","were unremarkable","lived a normal life"];
 
         //these only have the fields that would be interesting to have
-        //TODO give them more than empty lists
-        patienceFlavor = new StatFlavor()
+        patienceFlavor = new StatFlavor(-3.0)
             ..medium = <String>["gained a reputation for slow and steady excellence"];
-        energeticFlavor = new StatFlavor()
+        energeticFlavor = new StatFlavor(0.0)
             ..medium = <String>["brought a vibrant energy to everything they did"];
-        idealisticFlavor = new StatFlavor()
+        idealisticFlavor = new StatFlavor(1.0)
+            ..death  = <String>["fighting for what they believed in","trying to change the world","trying to change the empire"]
              ..medium = <String>["inspired the trolls around them with their ideals"];
-        curiousFlavor = new StatFlavor()
+        curiousFlavor = new StatFlavor(1.0)
+            ..death  = <String>["asking the wrong questions","probing into things better left alone","exposing the wrong highBlood's secrets"]
             ..medium = <String>["never stopped asking questions"];
-        loyalFlavor = new StatFlavor()
+        loyalFlavor = new StatFlavor(1.0)
+            ..death  = <String>["fighting the Empire's enemies","protecting their friends","putting down rebels and traitors"]
             ..medium = <String>["were a staunch supporter of the Empire"];
-        externalFlavor = new StatFlavor()
+        externalFlavor = new StatFlavor(1.0)
+            ..death  = <String>["getting into other troll's business too much","trying to manipulate the wrong Highblood","bugging and fussing and meddling with the wrong Highblood"]
             ..medium = <String>["investigated the world around them constantly"];
-        impatienceFlavor = new StatFlavor()
+        impatienceFlavor = new StatFlavor(1.0)
+            ..death  = <String>["skipping critical steps in a dangerous procedure","failing to read the instructions","trying to gain power too quickly"]
             ..medium = <String>["always rushed ahead to the next big thing"];
-        calmFlavor = new StatFlavor()
+        calmFlavor = new StatFlavor(-3.0)
             ..medium = <String>["made sure not to get too excited about unlikely possibilities"];
-        realisticFlavor = new StatFlavor()
+        realisticFlavor = new StatFlavor(0.0)
             ..medium = <String>["always strove to see the world for how it truly was"];
-        acceptingFlavor = new StatFlavor()
+        acceptingFlavor = new StatFlavor(-3.0)
             ..medium = <String>["collected unsolved mysteries"];
-        freeFlavor = new StatFlavor()
+        freeFlavor = new StatFlavor(1.0)
+            ..death  = <String>["rebelling against the empire","without any friends beside them","betraying the wrong Highblood"]
             ..medium = <String>["never stayed in any one place long"];
-        internalFlavor = new StatFlavor()
+        internalFlavor = new StatFlavor(0.0)
             ..medium = <String>["learned to be their true self"];
     }
 
@@ -94,6 +102,9 @@ class Stat {
 
 
     int get normalizedValue => value.abs();
+
+    //useful to know how 'big' this stat is
+    double get percentOfHighValue => (normalizedValue/Stat.HIGH);
 
     String get stringValue {
         if(normalizedValue > VERYFUCKINGHIGH) return "Insanely High";
@@ -138,6 +149,16 @@ class StatFlavor {
     List<String> fuchsia = new List<String>();
     List<String> purple = new List<String>();
     List<String> mutant = new List<String>();
+
+    //they died X "fighting the empire" "of a mysterious illness" etc.
+    List<String> death = new List<String>();
+
+    //if you're patient, reduces, if you're impatient,increases, etc
+    double oddsOfViolentDeath = 0.0;
+
+    StatFlavor(this.oddsOfViolentDeath) {
+
+    }
 
 
 
