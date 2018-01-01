719aec08b7b35bbef0799230104391f4c8a9d9e2
Jenny <gtg287y@gmail.com>
2017-12-31 19:28:31 -0500

    plannign items

---
+++ web/scripts/GameShit/AIItem.dart
@@ -0,0 +1,11 @@
+import "AIObject.dart";
+/*
+    has a string that is it's image file, a string that is it's name
+    stats.
+    when a grub interacts with it, it's stats will determine if they like it or not.
+
+    the item does not interact back
+ */
+class AIItem extends AIObject {
+
+}
