28589158e894fefd47babcd8af2f68346a08b5a4
Jenny <gtg287y@gmail.com>
2017-12-31 20:15:28 -0500

    items picked

--- web/scripts/GameShit/AIItem.dart
+++ web/scripts/GameShit/AIItem.dart
@@ -1,10 +1,14 @@
 import "AIObject.dart";
 /*
-    has a string that is it's image file, a string that is it's name
+    has a list of strings that are possible image files, a string that is it's name (generic troll word)
+    soft thing, living thing, food thing, hard thing
     stats.
     when a grub interacts with it, it's stats will determine if they like it or not.
 
     the item does not interact back
+
+    when buying an item, only unique slots for items with unique stats.
+    i.e. tildeath book and skull (hypothetically) both show in same slot, randomly???
  */
 class AIItem extends AIObject {
 
