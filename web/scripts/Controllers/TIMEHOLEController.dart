5c4c0858c5531a72af4d7dc5c1a75c5b14d1ab41
jadedResearcher <gtg287y@gmail.com>
2018-11-27 19:31:53 -0500

    emergency discouragement for adopting wigglers when hole is nearly empty

--- web/scripts/Controllers/TIMEHOLEController.dart
+++ web/scripts/Controllers/TIMEHOLEController.dart
@@ -252,6 +252,7 @@ void finishLoadingCount(String response)  {
    //the more wigglers there are the cheaper the cost
    int cost = 300-count*3;
    cost = Math.max(cost,13); //cost at least 13.
+   if (count < 30) cost = (game.player.caegers/2).round();
    if(game.player.caegers > cost) {
        ButtonElement button = new ButtonElement()..text = "Spend $cost caegers to selflessly adopt a wiggler from the TIMEHOLE?";
        output.append(button);
